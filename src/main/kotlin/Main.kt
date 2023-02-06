036083a74e7f6ebbfa91428b22c7a2902887b2af
EstherMwiyeria <esthermkanoro@gmail.com>
2023-02-06 14:50:25 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var names = arrayOf("Lucky", "Gisemba")
+    var phonenumbers = arrayOf(726699826, 748240785)
+    val nationalityKenyan = true
+    var details = arrayOf("details",names [0],phonenumbers[0],!nationalityKenyan)
+    var list = arrayOf("list", names[1], phonenumbers[1], nationalityKenyan)
+
+    println(details.contentToString())
+    println(list.contentToString())
+
+
+}
